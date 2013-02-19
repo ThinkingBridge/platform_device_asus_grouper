1a8a3fe659ff0751e4086c4c4e07c97e26770670
htchoi1995 <htchoi1995@gmail.com>
2013-02-19 23:30:42 +0900

    Remove cyanogenmod_grouper_defconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,6 +86,3 @@ BOARD_USES_GROUPER_MODULES := true
 
 # Required for CWM
 BOARD_HAS_NO_SELECT_BUTTON := true
-
-TARGET_KERNEL_SOURCE := kernel/asus/grouper
-TARGET_KERNEL_CONFIG := cyanogenmod_grouper_defconfig
