4eb96983fd68a16d71a8eb5d53be2f4ab3a47196
htchoi1995 <htchoi1995@gmail.com>
2014-01-09 14:06:41 +0900

    grouper: fixes for thinkingbridge

--- thinkingbridge.mk
+++ thinkingbridge.mk
@@ -5,18 +5,15 @@ PRODUCT_RELEASE_NAME := Nexus7
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common ThinkingBridge stuff.
+$(call inherit-product, vendor/thinkingbridge/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/grouper/full_grouper.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grouper
-PRODUCT_NAME := cm_grouper
+PRODUCT_NAME := thinkingbridge_grouper
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
